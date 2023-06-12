5dc7c4a294dc8f945af5a44151aaa07f3dcf2eb3
Samuel Yohannes <127985871+yosami14@users.noreply.github.com>
2023-06-12 14:54:17 +0300

    logic change and stylesheet modification

--- index.js
+++ index.js
@@ -1,94 +1,108 @@
   // user count 
-  let countOfUsers  = 2;
+  let countOfUsers  = 1;
   let totalContribution = 0;
   let userObject = {}
-function collectData() {
-  //our users data 
-  let firstName = document.getElementById("firstNameInput").value;
-  let fathersName = document.getElementById("fatherNameInput").value;
-  let email = document.getElementById("userEmailInput").value;
-  let phoneNumber = document.getElementById("userPhoneNumberInput").value;
-  let contributionAmount = document.getElementById("userContributionInput").value;
-  let gender = document.querySelector('input[name="userGender"]:checked').value;
 
+  // Add event listener to the button
+  document.getElementById("collectDataBtn").addEventListener("click", collectData);
 
-//year-month-date
-    // Get the current date
-    let currentDate = new Date();
+  function collectData() {
 
-    // Get the individual components of the date
-    let year = currentDate.getFullYear(); // 4-digit year (e.g., 2023)
-    let month = currentDate.getMonth() + 1; // Month number (1-12; 1 represents January)
-    let day = currentDate.getDate(); // Day of the month (1-31)
-
-    // Format the date as a string
-    let formattedDate = `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
-
-// Create an object
-
-  userObject[countOfUsers]= {
-  firstName: firstName,
-  fathersName: fathersName,
-  gender:gender,
-  email: email,
-  phoneNumber: phoneNumber,
-  contributionAmount: contributionAmount,
-  }
+    //our users data 
+    let firstName = document.getElementById("firstNameInput").value;
+    let fathersName = document.getElementById("fatherNameInput").value;
+    let email = document.getElementById("userEmailInput").value;
+    let phoneNumber = document.getElementById("userPhoneNumberInput").value;
+    let contributionAmount = document.getElementById("userContributionInput").value;
+    let gender = document.querySelector('input[name="userGender"]:checked').value;
 
 
 
 
+      //year-month-date
+      // Get the current date
+      let currentDate = new Date();
 
-let addUserRow = document.querySelector('#addUserRow');
-let createRow = document.createElement('tr');
-let createRowHead = document.createElement('th');
+      // Get the individual components of the date
+      let year = currentDate.getFullYear(); // 4-digit year (e.g., 2023)
+      let month = currentDate.getMonth() + 1; // Month number (1-12; 1 represents January)
+      let day = currentDate.getDate(); // Day of the month (1-31)
 
-//row dataHead
-createRowHead.setAttribute('scope', 'row');
-createRowHead.textContent = countOfUsers++;
-createRow.appendChild(createRowHead);
+      // Format the date as a string
+      let formattedDate = `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
 
-//row data
-  let lastObject = userObject[countOfUsers - 1];
+      // Create an object
 
-for (let property in lastObject) {
-  if (lastObject.hasOwnProperty(property)) {
-      let createRowData = document.createElement('td');
-      createRowData.textContent = lastObject[property];
-      createRow.appendChild(createRowData);
+        userObject[countOfUsers]= {
+        firstName: firstName,
+        fathersName: fathersName,
+        gender:gender,
+        email: email,
+        phoneNumber: phoneNumber,
+        contributionAmount: contributionAmount,
+        date: formattedDate
+        }
+
+
+        let addUserRow = document.querySelector('#addUserRow');
+        let createRow = document.createElement('tr');
+        createRow.classList.add([countOfUsers]);
+        let createRowHead = document.createElement('th');
+
+        // Row dataHead
+        createRowHead.setAttribute('scope', 'row');
+        createRowHead.textContent = countOfUsers++;
+        createRow.appendChild(createRowHead);
+
+        // Row data print
+        let lastObject = userObject[countOfUsers - 1];
+
+        for (let property in lastObject) {
+          if (lastObject.hasOwnProperty(property)) {
+            var createRowData = document.createElement('td');
+            createRowData.textContent = lastObject[property];
+            createRow.appendChild(createRowData);
+          }
+        }
+
+    // Add delete button
+    let deleteColumn = document.createElement('td');
+    deleteColumn.classList.add('delete');
+    deleteColumn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    createRow.appendChild(deleteColumn);
+
+    //event listener to delete button
+    deleteColumn.addEventListener('click', function() {
+      createRow.remove();
+    });
+
+    // Append the row to the table
+    addUserRow.appendChild(createRow);
+
+    // Update total contribution
+    totalContribution += parseInt(contributionAmount);
+
+    // Update total contribution display
+    let totalContributionElement = document.getElementById("displayTotal");
+    totalContributionElement.textContent = `Total: ${totalContribution}`;
+
+    // append the row to the table
+    addUserRow.appendChild(createRow);
+
+    //revile the table 
+    let displayTable = document.querySelector('.displayNon');
+    if (countOfUsers > 1) {
+    displayTable.classList.remove('displayNon');
+    } else {
+    displayTable.classList.add('displayNon');
     }
-  }
+    }
 
-
-
-// Update total contribution
-  totalContribution += parseInt(contributionAmount);
-
-  // Update total contribution display
-  let totalContributionElement = document.getElementById("displayTotal");
-  totalContributionElement.textContent = `Total: ${totalContribution}`;
-// append the row to the table
-addUserRow.appendChild(createRow);
-
-
-
-
-
-}
-// Add event listener to the button
-document.getElementById("collectDataBtn").addEventListener("click", collectData);
-
-
-// function winner() {
-//   let randomNum = Math.floor(Math.random() * countOfUsers);
-//   let winner = userObject[randomNum];
-//   console.log(winner.firstName);
-// }
 
 function winner() {
-  let randomNum = Math.floor(Math.random() * countOfUsers)+2;
-  let winner = userObject[randomNum];
   
+  let randomNum = Math.floor(Math.random() * countOfUsers)+1;
+  let winner = userObject[randomNum];
 
   //display the winner
   let modalBody = document.getElementsByClassName('modal-body')[0];
@@ -100,10 +114,16 @@ function winner() {
   winnerHeading.textContent = `${winner.firstName} ${winner.fathersName}`;
 
   // Append the <h2> element to the modal body
+  modalBody.textContent = ' ';
   modalBody.appendChild(winnerHeading);
 
+  // Remove previous highlight
+  let previousWinner = document.querySelector('.text-danger');
+  if (previousWinner) {
+    previousWinner.classList.remove('text-danger');
+  }
+
   //add higlight class
- let highlightWinner = document.querySelectorAll('#addUserRow tr')[randomNum-1];
- 
+  let highlightWinner = document.querySelectorAll('#addUserRow tr')[randomNum];
   highlightWinner.classList.add('text-danger');
 }
